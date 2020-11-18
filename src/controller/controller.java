4d22cb57f95485c7655a8aa490e52abce28d76e9
William Juniar <williamjuniar@gmail.com>
2020-11-18 11:07:16 +0700

    controller

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -5,12 +5,59 @@
  */
 package controller;
 
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.jurusan;
+import model.mahasiswa;
 
 /**
  *
  * @author ASUS
  */
 public class controller {
-
+    static DatabaseHandler conn = new DatabaseHandler();
+    
+    public static ArrayList<jurusan> getAllJurusan() {
+        ArrayList<jurusan> jurusan3 = new ArrayList<>();
+        conn.Connect();
+        String query = "SELECT * FROM jurusan";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                jurusan jurusan2 = new jurusan();
+                jurusan2.setKode(rs.getString("kodeJurusan"));
+                jurusan2.setNama(rs.getString("namaJurusan"));
+                jurusan3.add(jurusan2);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (jurusan3);
+    }
+       
+    public static ArrayList<jurusan> getKode() {
+        ArrayList<jurusan> jurusan3 = new ArrayList<>();
+        conn.Connect();
+        String query = "SELECT * FROM jurusan";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                jurusan jurusan2 = new jurusan();
+                jurusan2.setKode(rs.getString("kodeJurusan"));
+                jurusan3.add(jurusan2);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (jurusan3);
+    }
+   
+    
+   
+    
 }
